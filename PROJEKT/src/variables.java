ea00236cef2346c0a9b7e75a079fa354416d0844
ReneVajda1 <rene.vajda@yahoo.com>
2024-10-09 00:46:51 +0200

    var-test

---
+++ PROJEKT/src/variables.java
@@ -0,0 +1,16 @@
+import java.time.DayOfWeek;
+
+public class variables {
+
+    //Create a variable of type int called daysInWeek
+    // and set it to the correct value.
+    // Notice how we print the value to the terminal by putting
+    // the variable name inside of the print method's parentheses ( ).
+
+    public static void main(String[] args) {
+        int DaysInWeek = 7;
+        int DaysInMonth =30;
+        System.out.println(DaysInWeek+DaysInMonth);
+    }
+
+}

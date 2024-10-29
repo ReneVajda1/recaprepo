91f9a97d926fd4efdf54c4f00092f6bc19601aac
ReneVajda1 <rene.vajda@yahoo.com>
2024-10-29 02:57:24 +0100

    var exercises float

---
+++ PROJEKT/src/ValueTypes.java
@@ -0,0 +1,12 @@
+public class ValueTypes {
+    public static void main(String[] args) {
+        String hello = "Hello, World!";
+        System.out.println(hello);
+
+        int ft = 42;
+        System.out.println("int "+ft);
+
+        float fl = 3.141592F;
+        System.out.println("float "+fl);
+    }
+}

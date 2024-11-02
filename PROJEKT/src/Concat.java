31cabeb43a8f5d2e6434dc75e857088734d98d01
ReneVajda1 <rene.vajda@yahoo.com>
2024-11-02 23:51:20 +0100

    concat of strings

---
+++ PROJEKT/src/Concat.java
@@ -0,0 +1,8 @@
+public class Concat {
+    public static void main(String[] args) {
+        // String and special characters
+        System.out.println("apple");   // Prints apple
+        System.out.println("don't");   // Prints don't
+        System.out.println("\"Everything you can imagine is real.\" - Picasso"); // Prints "Everything you can imagine is real." - Picasso
+    }
+}
---
+++ PROJEKT/src/Cuboid.java
@@ -0,0 +1,15 @@
+public class Cuboid {
+    // Write a program that stores 3 sides of a cuboid as variables (doubles)
+    // The program should write the surface area and volume of the cuboid.
+    //
+    // Set the 3 sides to 10.4, 13.5, 8.2 and your program should
+    // produce the following output:
+    //
+    // Surface Area: 672.76
+    // Volume: 1151.28
+    public static void main(String[] args) {
+        double bottom = 10.4;
+        double side = 13.5;
+        double top =
+    }
+}

05ae603f6d8372195d308f2bac7d24a72634311e
ReneVajda1 <rene.vajda@yahoo.com>
2024-12-30 00:55:35 +0100

    enum parkingLot finished

--- PROJEKT/src/Enum/parkingLot/Car.java
+++ PROJEKT/src/Enum/parkingLot/Car.java
@@ -1,32 +1,33 @@
 package Enum.parkingLot;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Car {
-
-
-    //    Parking Lot
-//    Create a Car class with 2 enum properties
-//    enum of car types
-//    The types of these cars or vehicles is up to you
-//    enum of colors
-//    Create 256 Vehicles randomly and put them into a List
-//    Count and Print the number of same vehicles for each type
-//    Count and Print the number of same vehicles for each color
-//    Print the most frequently occurring vehicle
-    public enum carTypes{
-    AUDI, LADA, OPEL, VOLKSWAGEN, BMW
+    public enum carTypes {
+        AUDI, LADA, OPEL, VOLKSWAGEN, BMW
     }
-    public enum carColors{
+
+    public enum carColors {
         BLACK, WHITE, BLUE, GREEN, ORANGE, CRIMSON
     }
 
-    public Car(carTypes carTypes, carColors carColors, carTypes type, carColors colors) {
+    private final carTypes type;
+    private final carColors color;
+
+    // Constructor
+    public Car(carTypes type, carColors color) {
         this.type = type;
-        this.colors = colors;
+        this.color = color;
     }
 
-    private final carTypes type;
-    private final carColors colors;
+    public carTypes getType() {
+        return type;
+    }
+
+    public carColors getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return type + " (" + color + ")";
+    }
 }
--- PROJEKT/src/Enum/parkingLot/Main.java
+++ PROJEKT/src/Enum/parkingLot/Main.java
@@ -1,25 +1,58 @@
 package Enum.parkingLot;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+
+import java.util.*;
+
 public class Main {
-
-
     public static void main(String[] args) {
         List<Car> listOfCars = new ArrayList<>();
-         Random random = new Random();
+        Random random = new Random();
+
+        // Create 256 cars randomly
         for (int i = 0; i < 256; i++) {
             Car.carTypes randomType = Car.carTypes.values()[random.nextInt(Car.carTypes.values().length)];
             Car.carColors randomColor = Car.carColors.values()[random.nextInt(Car.carColors.values().length)];
-
             listOfCars.add(new Car(randomType, randomColor));
         }
 
-        System.out.println("asdf string");
-        for (int i = 0; i < listOfCars.size(); i++) {
-            System.out.println(listOfCars.get(i));
+        // Count the number of cars of each type
+        Map<Car.carTypes, Integer> typeCount = new HashMap<>();
+        for (Car car : listOfCars) {
+            typeCount.put(car.getType(), typeCount.getOrDefault(car.getType(), 0) + 1);
+        }
 
+        System.out.println("Count of cars by type:");
+        for (Map.Entry<Car.carTypes, Integer> entry : typeCount.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        // Count the number of cars of each color
+        Map<Car.carColors, Integer> colorCount = new HashMap<>();
+        for (Car car : listOfCars) {
+            colorCount.put(car.getColor(), colorCount.getOrDefault(car.getColor(), 0) + 1);
+        }
+
+        System.out.println("\nCount of cars by color:");
+        for (Map.Entry<Car.carColors, Integer> entry : colorCount.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        // Find the most frequently occurring car type
+        Car.carTypes mostFrequentType = null;
+        int maxTypeCount = 0;
+
+        for (Map.Entry<Car.carTypes, Integer> entry : typeCount.entrySet()) {
+            if (entry.getValue() > maxTypeCount) {
+                maxTypeCount = entry.getValue();
+                mostFrequentType = entry.getKey();
+            }
+        }
+
+        System.out.println("\nMost frequently occurring car type: " + mostFrequentType + " with " + maxTypeCount + " cars.");
+
+        // Print all the cars
+        System.out.println("\nList of all cars:");
+        for (Car car : listOfCars) {
+            System.out.println(car);
         }
     }
 }
-

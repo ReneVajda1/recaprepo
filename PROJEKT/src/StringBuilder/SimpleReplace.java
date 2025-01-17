0c60f796bfc29b0480bbe3d9b38d3866370381df
ReneVajda1 <rene.vajda@yahoo.com>
2025-01-17 02:58:34 +0100

    random Stringbuilder exercise

---
+++ PROJEKT/src/StringBuilder/SimpleReplace.java
@@ -0,0 +1,10 @@
+package StringBuilder;
+
+public class SimpleReplace {
+    public static void main(String[] args) {
+        String example = "in a dishwasher far, far away";
+        example = example.replace("dishwasher","galaxy");
+        System.out.println(example);
+    }
+
+}

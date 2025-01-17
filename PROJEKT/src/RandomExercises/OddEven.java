a7810cd19385bfe1bdc0fda8cd49d05130182fd8
ReneVajda1 <rene.vajda@yahoo.com>
2025-01-17 03:01:37 +0100

    OddEven number % 2 == 0

---
+++ PROJEKT/src/RandomExercises/OddEven.java
@@ -0,0 +1,13 @@
+package RandomExercises;
+
+public class OddEven {
+    public static void main(String[] args) {
+
+        int number = 4;
+        if(number % 2 != 0) {
+            System.out.println("number " + number + " is odd");
+        } else {
+            System.out.println("number " + number + " is even");
+        }
+    }
+}

570ad00c7e0644c5f045631ee520b8d3f49d4f5f
ReneVajda1 <rene.vajda@yahoo.com>
2025-01-18 23:56:13 +0100

    total recap

---
+++ PROJEKT/src/expressionControlFlow/TotalBasics.java
@@ -0,0 +1,28 @@
+package expressionControlFlow;
+
+public class TotalBasics {
+    public static void main(String[] args) {
+        int a,b,c,d;
+        a = 10;
+        b = 20;
+
+        c = (a + b) /10;
+
+        d = c*b;
+
+        System.out.println(d);
+
+        byte verySmallNumber;
+        short smallNumber;
+        int averageNumber;
+        long largeNumber;
+
+        float averageDecimal = 1.023123124f;  // here f in the end
+        double largeDecimal = 122.43425345;
+
+        boolean trueOrFalse = true;
+
+        char character = 's';
+        String arrayOfchar = "asdfasdf";
+    }
+}
---
+++ PROJEKT/src/expressionControlFlow/TotalBasicsBooleanOperatorsAndIfStatement.java
@@ -0,0 +1,31 @@
+package expressionControlFlow;
+
+public class TotalBasicsBooleanOperatorsAndIfStatement {
+    public static void main(String[] args) {
+
+        // Equal to                 ==      checks if value is equal with double ==
+        // one                      =       assigns the value of left side to the right   >>> boolean a = false;
+
+        // Greater than             >
+        // Grather than/equal to    >=
+
+        // Less than                <
+        // less than/ equal to      <=
+
+
+        // Not equal to             !=
+
+        //        boolean a = (2 != 4);
+        //        System.out.println(a);  // true
+
+        //        if( condition is true) {
+        //            do this code here
+        //        }
+        boolean a = (11 -1 == 10);
+        if(a) {
+            System.out.println("condition is true");
+        }
+
+
+    }
+}

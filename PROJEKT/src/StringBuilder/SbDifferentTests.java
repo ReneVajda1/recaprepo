e5b75f87c5ce4a67676cb528bee2cf376d7ba015
ReneVajda1 <rene.vajda@yahoo.com>
2025-01-26 23:40:31 +0100

    Stringbuilder function3

--- PROJEKT/src/StringBuilder/SbDifferentTests.java
+++ PROJEKT/src/StringBuilder/SbDifferentTests.java
@@ -4,22 +4,29 @@ public class SbDifferentTests {
     public static void main(String[] args) {
         String string = "hi";
 
-        hello(string);
-        System.out.println(hellohi(string));
-
+        hello1(string);
+        System.out.println(hello2(string));
+        hello3(string);
+      
     }
 
-    public static void hello(String input){
+    public static void hello1(String input){
         StringBuilder sb = new StringBuilder(input);
 
         sb.append(" hello");
         System.out.println(sb.toString());
     }
 
-    public static StringBuilder hellohi(String input){
+    public static StringBuilder hello2(String input){
         StringBuilder sb = new StringBuilder(input);
         sb.append(" HelloHI");
 
         return sb;
+    }
+
+    public static void hello3(String input){
+        StringBuilder sb = new StringBuilder(input);
+        sb.append(" helloooo");
+        System.out.println(sb.append(" helloooo") + sb.toString());
     }
 }

6e521d61e828ed6271587e0fd4311a3675b6cbbb
ReneVajda1 <rene.vajda@yahoo.com>
2025-03-15 00:11:48 +0100

    DiagonalStar

--- PROJEKT/src/JavaMasterClass/DiagonalStar.java
+++ PROJEKT/src/JavaMasterClass/DiagonalStar.java
@@ -1,4 +1,26 @@
 package JavaMasterClass;
 
 public class DiagonalStar {
+
+    public static void printSquareStar(int number){
+        if (number < 5 ){
+            System.out.println("invalud value");
+            return;
+        }
+
+
+        for (int row = 1; row <= number ; row++) {
+            for (int col = 1; col <= number ; col++) {
+                if (row == 1 || row == number || col == 1 || col == number || row == col || col == (number - row + 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }System.out.println(" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        printSquareStar(5);
+    }
 }

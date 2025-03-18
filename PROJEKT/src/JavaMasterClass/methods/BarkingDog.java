a1ad780f6a569c08a0413398d034541f2b772adf
ReneVajda1 <rene.vajda@yahoo.com>
2025-03-18 23:01:06 +0100

    methods - BarkingDog

---
+++ PROJEKT/src/JavaMasterClass/methods/BarkingDog.java
@@ -0,0 +1,20 @@
+package JavaMasterClass.methods;
+
+public class BarkingDog {
+    public static void main(String[] args) {
+        System.out.println(shouldWakeUP2(true,1));
+        System.out.println(shouldWakeUP2(false,2));
+        System.out.println(shouldWakeUP2(true,8));
+        System.out.println(shouldWakeUP2(true,-1));
+    }
+
+    public static boolean shouldWakeUP2(boolean barking, int hoursOfDay) {
+        if (hoursOfDay < 0 || hoursOfDay > 23) {
+            return false;
+        }
+        if (barking && (hoursOfDay < 8 || hoursOfDay > 22)) {
+            return true;
+        }
+        return false;
+    }
+}

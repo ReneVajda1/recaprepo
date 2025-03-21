4a92db5f329cd8de7325332b43e94ae13cd88ccf
ReneVajda1 <rene.vajda@yahoo.com>
2025-03-21 04:06:16 +0100

    methods - Overloading - IntchesToCmConvertor

--- PROJEKT/src/JavaMasterClass/methods/OverloadingCmConverter.java
+++ PROJEKT/src/JavaMasterClass/methods/OverloadingCmConverter.java
@@ -1,4 +1,24 @@
 package JavaMasterClass.methods;
 
 public class OverloadingCmConverter {
+    public static void main(String[] args) {
+        System.out.println(convertToCentimeters(10.5));
+        System.out.println(heightInFeets(6,5));
+    }
+
+    public static double convertToCentimeters(int entireHeightInInches){
+        return entireHeightInInches * 2.54;
+    }
+
+    public static double convertToCentimeters(double entireHeightInInches){
+        return entireHeightInInches * 2.54;
+    }
+
+    public static double heightInFeets(int heightInFeet, int remainingHeightInInches){
+        double convertedToInchesOnly = convertToCentimeters(heightInFeet
+                * 12 + remainingHeightInInches);
+        return convertedToInchesOnly;
+    }
+
+
 }

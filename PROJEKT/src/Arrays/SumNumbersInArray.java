1dd7c4ee81c1bda1d108bd9d9de77bc0819306c7
ReneVajda1 <rene.vajda@yahoo.com>
2025-10-27 23:11:05 +0100

    recap of random

--- PROJEKT/src/Arrays/SumNumbersInArray.java
+++ PROJEKT/src/Arrays/SumNumbersInArray.java
@@ -1,2 +1,18 @@
-package Arrays;public class SumNumbersInArray {
+package Arrays;
+
+import java.util.Arrays;
+
+
+public class SumNumbersInArray {
+    public static void main(String[] args) {
+
+        int[] sumNum = {10,20,30,40};
+
+        for (int i = 0; i < sumNum.length; i++) {
+
+        }
+        System.out.println(Arrays.stream(sumNum).sum());
+
+
+    }
 }
